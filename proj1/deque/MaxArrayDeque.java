fdc3a35c2192f9677abcdf33feb46d5d6e0a2300
Minuetcat <minuetcat@qq.com>
2026-07-09 01:30:26 +0800

    Create MaxArrayDeque.java

---
+++ proj1/deque/MaxArrayDeque.java
@@ -0,0 +1,36 @@
+package deque;
+
+import java.util.Comparator;
+
+public class MaxArrayDeque<T> extends ArrayDeque<T> {
+    private Comparator<T> comparator;
+    public MaxArrayDeque(Comparator<T> c) {
+        this.comparator = c;
+    }
+    public T max() {
+        if (size() == 0) {
+            return null;
+        }
+        T maxItem = get(0);
+        for (int i = 1; i < size(); i++) {
+            T current = get(i);
+            if (comparator.compare(current, maxItem) > 0) {
+                maxItem = current;
+            }
+        }
+        return maxItem;
+    }
+    public T max(Comparator<T> c) {
+        if (size() == 0) {
+            return null;
+        }
+        T maxItem = get(0);
+        for (int i = 1; i < size(); i++) {
+            T current = get(i);
+            if (c.compare(current, maxItem) > 0) {
+                maxItem = current;
+            }
+        }
+        return maxItem;
+    }
+}
