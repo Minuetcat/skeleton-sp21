2795c4ef1a9894a908a672c853aa31757c9a4c11
Minuetcat <minuetcat@qq.com>
2026-07-09 01:30:30 +0800

    Update LinkedListDeque.java

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -2,7 +2,7 @@ package deque;
 
 import java.util.Iterator;
 
-public class LinkedListDeque<T> implements Deque<T> {
+public class LinkedListDeque<T> implements Deque<T>, Iterable<T> {
     // 节点类
     private class Node {
         T item;
