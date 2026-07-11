6e78666185ef43cf89dab78d5dd78eaf941e2eab
Minuetcat <minuetcat@qq.com>
2026-07-12 02:21:55 +0800

    Update Commit.java

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -1,52 +1,54 @@
 package gitlet;
 
+import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
-import static gitlet.Utils.*;
-import java.io.Serializable;
 import java.util.Locale;
 
-/** Represents a gitlet commit object.
- *  A class about commit for gitlet.
- *  does at a high level.
- *
+import static gitlet.Utils.sha1;
+
+/** Represents a Gitlet commit object.
  *  @author minuet
  */
 public class Commit implements Serializable {
-    /**
-     * Add instance variables here.
-     *
-     * List all instance variables of the Commit class here with a useful
-     * comment above them describing what that variable represents and how that
-     * variable is used. We've provided one example for `message`.
-     */
     private String timestamp;
     private String parentId;
+    private String secondParentId;
     private HashMap<String, String> trackedFiles;
-
-    /** The message of this Commit. */
     private String message;
 
     public Commit() {
         this.message = "initial commit";
         this.timestamp = "Thu Jan 1 00:00:00 1970 +0000";
         this.parentId = null;
+        this.secondParentId = null;
         this.trackedFiles = new HashMap<>();
     }
 
-    public Commit(String message, String parentId, HashMap<String, String> trackedFiles) {
+    public Commit(String message, String parentId,
+                  HashMap<String, String> trackedFiles) {
+        this(message, parentId, null, trackedFiles);
+    }
+
+    public Commit(String message, String parentId, String secondParentId,
+                  HashMap<String, String> trackedFiles) {
         this.message = message;
         Date date = new Date();
-        SimpleDateFormat formatter = new SimpleDateFormat("EEE MMM d HH:mm:ss yyyy Z", Locale.US);
+        SimpleDateFormat formatter =
+                new SimpleDateFormat("EEE MMM d HH:mm:ss yyyy Z", Locale.US);
         this.timestamp = formatter.format(date);
         this.parentId = parentId;
+        this.secondParentId = secondParentId;
         this.trackedFiles = new HashMap<>(trackedFiles);
     }
 
     public String getID() {
         String parentForHash = parentId == null ? "" : parentId;
-        return sha1(message, timestamp, parentForHash, trackedFiles.toString());
+        String secondParentForHash =
+                secondParentId == null ? "" : secondParentId;
+        return sha1(message, timestamp, parentForHash,
+                secondParentForHash, trackedFiles.toString());
     }
 
     public HashMap<String, String> getTrackedFiles() {
@@ -63,5 +65,9 @@ public class Commit implements Serializable {
 
     public String getParentId() {
         return parentId;
+    }
+
+    public String getSecondParentId() {
+        return secondParentId;
     }
 }
